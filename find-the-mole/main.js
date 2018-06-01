179327faef5f942bc94dfb96b0d3bdee8a97d13f
dburns1 <patriciadburns@gmail.com>
2018-06-01 12:31:03 -0700

    randomizer Function

--- find-the-mole/main.js
+++ find-the-mole/main.js
@@ -1,7 +1,5 @@
 $(document).ready(initializeApp);
 
-
-
 function initializeApp(){
 console.log ('this is working'); 
 showMole(); 
@@ -25,9 +23,30 @@ function resetGame(){
 // once the game is over, reset all variables to 0 and start the game over again
 }
 
-function showMole () {
-   var randomNumber= [Math.floor(Math.random() * 4)];
-   randomNumber= randomNumber + 1000; 
-    setTimeout(popUpAnimation,randomNumber);
+function getRandomNumber(min, max) {
+   return (Math.floor(Math.random() * (max-min + 1))); 
 }
 
+
+function popUpAnimation(){
+    console.log("pop UP animation is firing"); 
+}
+
+
+function initiateMole(element, minTime, maxTime){
+    element = $(element);
+    var nextTime = getRandomNumber(minTime, maxTime);
+    var timer = null;
+    
+    
+    function toggleState(){
+      element.toggleClass('slide_animation');
+      nextTime = getRandomNumber(minTime, maxTime);
+      timer = setTimeout( toggleState, nextTime );//each successive timeout
+    }
+    
+    timer = setTimeout( toggleState, nextTime );//start the first timeout
+  }
+  
+// initiateMole(".mole",500,1000); 
+// initiateMole(".mole",500,2000); 
