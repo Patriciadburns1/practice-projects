fe2526c6571c756c8392e8f392dad979ac49611b
dburns1 <patriciadburns@gmail.com>
2018-06-01 11:22:48 -0700

    main.js changes

--- find-the-mole/main.js
+++ find-the-mole/main.js
@@ -3,10 +3,11 @@ $(document).ready(initializeApp);
 
 
 function initializeApp(){
-console.log ('this is working' )
+console.log ('this is working'); 
+showMole(); 
 }
 
-function clickHanlders(){
+function clickHandlers(){
 //onclick for when the user clicks the mole and wins 
 }
 
@@ -22,5 +23,11 @@ function winStatus(){
 
 function resetGame(){
 // once the game is over, reset all variables to 0 and start the game over again
+}
+
+function showMole () {
+   var randomNumber= [Math.floor(Math.random() * 4)];
+   randomNumber= randomNumber + 1000; 
+    setTimeout(popUpAnimation,randomNumber);
 }
 
