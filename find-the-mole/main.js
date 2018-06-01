d10e7ca476471abedc34ee54dc0a57bed86de926
schun92 <sarahsandiferchun@gmail.com>
2018-06-01 10:58:29 -0700

    added notes on mainjs

---
+++ find-the-mole/main.js
@@ -0,0 +1,26 @@
+$(document).ready(initializeApp);
+
+
+
+function initializeApp(){
+console.log ('this is working' )
+}
+
+function clickHanlders(){
+//onclick for when the user clicks the mole and wins 
+}
+
+
+function randomizeMole(){
+//pauls animation will be running and timed randomly 
+}
+
+
+function winStatus(){
+// when the user clicks the mole, some kind of alert 
+}
+
+function resetGame(){
+// once the game is over, reset all variables to 0 and start the game over again
+}
+
